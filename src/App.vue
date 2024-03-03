630d5fdab633c2267e4ea98486022b05ead7b48c
darina9 <111124490+darina9@users.noreply.github.com>
2024-03-03 20:12:26 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <MainPage /> <!-- Здесь используем компонент Page -->
+  </div>
+</template>
+
+<script>
+import MainPage from './view/MainPage.vue'; // Импорт компонента Page.vue
+
+export default {
+  name: 'App',
+  components: {
+    MainPage, // Регистрация компонента Page
+  },
+};
+</script>
+
+<style>
+/* Стили для корневого компонента приложения */
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue';
+import App from './App.vue';
+import store from './store';
+import VueI18n from 'vue-i18n';
+
+
+Vue.config.productionTip = false;
+
+Vue.use(VueI18n);
+
+const messages = {
+  ru: require('./locales/ru.json'),
+  en: require('./locales/en.json'),
+  es: require('./locales/es.json')
+};
+
+const i18n = new VueI18n({
+  locale: 'ru', // начальный язык
+  fallbackLocale: 'ru', // язык по умолчанию
+  messages
+});
+
+
+new Vue({
+  i18n,
+  store,
+  
+  render: h => h(App)
+}).$mount('#app');
